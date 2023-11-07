ff658ef11c53d33f62177288fe4e1577f7c16446
Sergei Khramtsov <vocmarh1989@gmail.com>
2023-11-08 00:19:05 +0500

    Update

--- CheckBox/MainViewViewWindow.cs
+++ CheckBox/MainViewViewWindow.cs
@@ -23,6 +23,7 @@ namespace CheckBox
         private ObservableCollection<ElementData> data;
         private ObservableCollection<CategoryModel> categories;
         private bool isSelected = true;
+        private string _searchText;
         public bool IsSelected
         {
             get { return isSelected; }
@@ -38,8 +39,25 @@ namespace CheckBox
             get => categories;
             set { categories = value; NotifyPropertyChanged(nameof(Categories));}
         }
+        public string SearchText
+        {
+            get { return _searchText; }
+            set { _searchText = value; NotifyPropertyChanged(nameof(SearchText)); }
+        }
+
+        private bool filterSearchText(object item)
+        {
+            CategoryModel catModel = (CategoryModel)item;
+            if (SearchText != null || SearchText != "")
+            {
+                return catModel.CategoryName.ToUpper().Contains(SearchText.ToUpper());
+
+            }
+            else { return true; }
+        }
 
         public DelegateCommand ExportExcel { get; }
+        public DelegateCommand SearchCategoryCommand { get; }
 
         private void NotifyPropertyChanged(string v)
         {
@@ -54,15 +72,54 @@ namespace CheckBox
             GetDataBase = new DelegateCommand(OnGetDataBase);
             Categories = new ObservableCollection<CategoryModel>();
             ExportExcel = new DelegateCommand(OnExportExcel);
+            SearchCategoryCommand = new DelegateCommand(OnSearchCategoryCommand);
 
             ListOfCategories listOfCategories = new ListOfCategories();
             listOfCategories.GetCategories(_commandData, Categories);            
+        }
+
+        private void OnSearchCategoryCommand()
+        {
+            // Получите введенный текст из SearchText
+            //string searchText = SearchText;
+
+            // Выполните поиск категории по имени в коллекции Categories
+            CategoryModel foundCategory = Categories.FirstOrDefault(category => category.CategoryName.Contains(_searchText));
+
+            // Далее, вы можете обновить ваш интерфейс или выполнить другие действия в зависимости от найденной категории
+            if (foundCategory != null)
+            {
+                // Категория найдена
+                // Дополнительные действия, например, установка флага IsSelected для категории и обновление интерфейса
+                foundCategory.IsSelected = true; // Пример установки флага IsSelected
+            }            
         }
 
         private void OnExportExcel()
         {
             ExcelExporters exporters = new ExcelExporters();
             exporters.GetElementToExcel(Data);
+        }
+        private void SearchCategory()
+        {
+            // Получите введенный текст из SearchText
+            string searchText = SearchText;
+
+            // Выполните поиск категории по имени в коллекции Categories
+            CategoryModel foundCategory = Categories.FirstOrDefault(category => category.CategoryName.Contains(searchText));
+
+            // Далее, вы можете обновить ваш интерфейс или выполнить другие действия в зависимости от найденной категории
+            if (foundCategory != null)
+            {
+                // Категория найдена
+                // Дополнительные действия, например, установка флага IsSelected для категории и обновление интерфейса
+                foundCategory.IsSelected = true; // Пример установки флага IsSelected
+            }
+            else
+            {
+                // Категория не найдена
+                // Дополнительные действия, если необходимо
+            }
         }
 
         private void OnGetDataBase()
